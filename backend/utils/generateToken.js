8657193665baf61d376f226986fa0b78c3a8c081
Ravichandra Reddy.G <chandrareddyravi134@gmail.com>
2025-06-07 16:23:36 +0530

    1st part is completed - backend

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+const generateTokenAndSetCookie = (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "15d",
+    });
+
+
+  res.cookie("jwt", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in milliseconds
+    httpOnly: true, //prevents client-side JavaScript from accessing the cookie
+    sameSite:"strict" // CSRF attacks cross-site request forgery attacks
+  });
+}
+export default generateTokenAndSetCookie;
